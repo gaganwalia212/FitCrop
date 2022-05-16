b7426f7d5ac3ff00c2cdde043153eb62e9969d49
Gagan <gaganwalia212@gmail.com>
2022-05-16 14:45:19 +0530

    add list of diseases

---
+++ Utilities/disease_list.py
@@ -0,0 +1,15 @@
+cherry_list=['Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy']
+apple_list=['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy']
+blueberry_list=['Blueberry___healthy']
+corn_list=['Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy']
+grape_list=['Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy']
+orange_list=['Orange___Haunglongbing_(Citrus_greening)']
+peach_list=['Peach___Bacterial_spot', 'Peach___healthy']
+pepperbell_list=['Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy']
+potato_list=['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+raspberry_list=['Raspberry___healthy']
+tomato_list=['Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
+
+print(potato_list)
+print(tomato_list[0:3])
+print(cherry_list[1])
